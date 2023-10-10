d2f5de44be6dfd4497da81a7350cd4b082b6bdb8
guga56 <gustavogregorio086@gmail.com>
2023-10-10 11:46:27 -0300

    desenvolvendo DTO do projeto

---
+++ ProjetoAPICliente.Data/DTO/ClienteDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAPICliente.Data.DTO
+{
+    public class ClienteDTO
+    {
+        public int IdCliente { get; set; }
+.
+        public string NomeCliente { get; set; }
+
+        public string EmailCliente { get; set; }
+    }
+}
